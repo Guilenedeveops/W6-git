4c3ddd006b90b041a5849d4849ef1b089f9a80ba
Guilenedeveops <Gpascale.fusillier@gmail.com>
2024-11-12 19:53:55 +0100

    ticket number

---
+++ version.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "harshicorp/aws"
+        version = "4.67.0"
+
+      }
+    }
+  }
+
+  provider "aws" {
+    region = "us-east-1"
+    
+  }
